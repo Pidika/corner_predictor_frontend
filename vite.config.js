2245b7bd5fc821bc5845a0c167f77e9cd4323316
Pidika <princeidika2@gmail.com>
2025-07-30 02:35:41 +0100

    updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-     tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || '/corner_predictor_frontend',
 })
 
 
-// export default defineConfig({
-//   plugins: [
-   
-//   ],
-// })
